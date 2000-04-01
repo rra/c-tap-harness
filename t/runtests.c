5627612a3b12769654c479e45eceb4f6992004b5
Russ Allbery <rra@stanford.edu>
2000-04-01 22:29:01 +0000

    Added reporting of execution and charged CPU time, added reporting of non-zero exit statuses from tests, allow test counts of the form "1..n" for compatibility with Perl's Test::Harness, removed an arbitrary limit on the test count, send all stderr output from tests to /dev/null, added preliminary support for skipped tests, improved the error handling in general, and changed the exit status to match the return status from test_batch().

--- t/runtests.c
+++ t/runtests.c
@@ -10,7 +10,7 @@
 
    Usage:
 
-        test_batch(file);
+        runtests <test-list>
 
    Expects a list of executables located in the given file, one line per
    executable.  For each one, runs it as part of a test suite, reporting
@@ -28,14 +28,19 @@
 
 #include "config.h"
 
+#include <ctype.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+/* sys/time.h must be included before sys/resource.h on some platforms. */
+#include <sys/resource.h>
 
 #include "error.h"
 #include "xmalloc.h"
@@ -55,56 +60,113 @@ enum test_status {
 
 /* Structure to hold data for a set of tests. */
 struct testset {
+    const char *file;           /* The file name of the test. */
     int count;                  /* Expected count of tests. */
+    int current;                /* The last seen test number. */
     int passed;                 /* Count of passing tests. */
     int failed;                 /* Count of failing lists. */
     int skipped;                /* Count of skipped tests (passed). */
     enum test_status *results;  /* Table of results by test number. */
-    const char *file;           /* The file name of the test. */
+    int aborted;                /* Whether the set as aborted. */
+    int status;                 /* The exit status of the test. */
 };
 
 /* Internal prototypes. */
 static int test_batch(const char *testlist);
-static int test_init(const char *line, struct testset *ts);
-static pid_t test_start(const char *path, const char *errfile, int *fd);
-static int test_finish(int fd, pid_t pid);
+static enum test_status test_checkline(const char *line, struct testset *);
+static int test_init(const char *line, struct testset *);
+static pid_t test_start(const char *path, int *fd);
+static double tv_diff(const struct timeval *, const struct timeval *);
+static double tv_seconds(const struct timeval *);
+static double tv_sum(const struct timeval *, const struct timeval *);
+static void wstat_print(int status);
+
+
+/* Given a struct timeval, return the number of seconds it represents as a
+   double.  Use difftime() to convert a time_t to a double. */
+static double
+tv_seconds(const struct timeval *tv)
+{
+    return difftime(tv->tv_sec, 0) + tv->tv_usec * 1e-6;
+}
+
+/* Given two struct timevals, return the difference in seconds. */
+static double
+tv_diff(const struct timeval *tv1, const struct timeval *tv0)
+{
+    return tv_seconds(tv1) - tv_seconds(tv0);
+}
+
+/* Given two struct timevals, return the sum in seconds as a double. */
+static double
+tv_sum(const struct timeval *tv1, const struct timeval *tv2)
+{
+    return tv_seconds(tv1) + tv_seconds(tv2);
+}
+
+
+/* Given a return status from waitpid(), print to stdout a human-readable
+   description of the exit status. */
+static void
+wstat_print(int status)
+{
+    if (WIFEXITED(status)) {
+        switch (WEXITSTATUS(status)) {
+        case CHILDERR_DUP:
+            puts("dup of file descriptors failed");
+            break;
+        case CHILDERR_EXEC:
+            puts("Execution of test script failed");
+            break;
+        case CHILDERR_STDERR:
+            puts("Unable to open /dev/null");
+            break;
+        default:
+            printf("Test exited with status %d\n", WEXITSTATUS(status));
+            break;
+        }
+    } else if (WIFSIGNALED(status)) {
+        printf("Test terminated with signal %d%s\n", WTERMSIG(status),
+               WCOREDUMP(status) ? " (core dumped)" : "");
+    }
+}
 
 
 /* Read the first line of test output, which should contain the range of
-   test numbers, and initialize the testset structure except for the name
-   (which should already be set).  Return true if initialization succeeded,
-   false otherwise.  No more than 10,000 tests are allowed in one set. */
+   test numbers, and initialize the testset structure.  Assume it was zeroed
+   before being passed in.  Return true if initialization succeeds, false
+   otherwise. */
 static int
 test_init(const char *line, struct testset *ts)
 {
-    int *results;
     int i;
 
+    /* Prefer a simple number of tests, but if the count is given as a range
+       such as 1..10, accept that too for compatibility with Perl's
+       Test::Harness. */
+    while (isspace((unsigned char)(*line))) line++;
+    if (!strncmp(line, "1..", 3)) line += 3;
+
+    /* Get the count, check it for validity, and initialize the struct. */
     i = atoi(line);
-    if (!i) {
-        warn("%s: invalid test count: %s", ts->file, line);
-        return 0;
-    }
-    if (i > 10000) {
-        warn("%s: claimed %d tests, maximum is 10,000", ts->file, i);
+    if (i <= 0) {
+        puts("invalid test count");
+        ts->aborted = 1;
         return 0;
     }
     ts->count = i;
-    ts->passed = 0;
-    ts->failed = 0;
-    ts->skipped = 0;
-    ts->results = xmalloc(ts->count * sizeof(int));
+    ts->results = malloc(ts->count * sizeof(enum test_status));
+    if (!ts->results) sysdie("can't allocate memory");
     for (i = 0; i < ts->count; i++) ts->results[i] = TEST_INVALID;
     return 1;
 }
 
 
-/* Start a program, connecting its stdout to a pipe on our end, its stderr
-   to the given file name, and storing the file descriptor to read from in
-   the third argument.  Returns the PID of the new process.  Errors are
-   fatal. */
+/* Start a program, connecting its stdout to a pipe on our end and its
+   stderr to /dev/null, and storing the file descriptor to read from in the
+   two argument.  Returns the PID of the new process.  Errors are fatal. */
 static pid_t
-test_start(const char *path, const char *errfile, int *fd)
+test_start(const char *path, int *fd)
 {
     int fds[2], errfd;
     pid_t child;
@@ -115,7 +177,7 @@ test_start(const char *path, const char *errfile, int *fd)
         sysdie("can't fork");
     } else if (child == 0) {
         /* In child.  Set up our stdout and stderr. */
-        errfd = open(errfile, O_WRONLY | O_CREAT | O_APPEND, 0666);
+        errfd = open("/dev/null", O_WRONLY);
         if (errfd < 0) _exit(CHILDERR_STDERR);
         if (dup2(errfd, 2) == -1) _exit(CHILDERR_DUP);
         close(fds[0]);
@@ -132,20 +194,39 @@ test_start(const char *path, const char *errfile, int *fd)
 }
 
 
-/* Finish off a child process behind the given file descriptor with the
-   given PID.  Closes the file descriptor and waits for the child, returning
-   its exit status in the same format as the second argument to waitpid.
-   Errors are fatal. */
-static int
-test_finish(int fd, pid_t pid)
+/* Given a single line of output from a test, parse it and return the
+   success status of that test.  Print an error followed by a newline to
+   stdout and return TEST_INVALID in the case of some parsing error.  Sets
+   current to the test number that just reported status. */
+static enum test_status
+test_checkline(const char *line, struct testset *ts)
 {
-    pid_t child;
-    int status;
+    enum test_status status = TEST_INVALID;
+    int failed, current;
 
-    close(fd);
-    child = waitpid(pid, &status, 0);
-    if (child == (pid_t) -1)
-        sysdie("waitpid for %u failed", (unsigned int) pid);
+    /* If the given line isn't newline-terminated, it was too big for an
+       fgets(), which means invalid output. */
+    if (line[strlen(line) - 1] != '\n') {
+        puts("excessive output");
+    } else if (sscanf(line, "not ok %d", &current) == 1) {
+        status = TEST_FAIL;
+    } else if (sscanf(line, "ok %d", &current) == 1) {
+        status = strstr(line, "# skip") ? TEST_SKIP : TEST_PASS;
+    } else {
+        puts("invalid output");
+    }
+
+    if (status == TEST_INVALID) return status;
+
+    if (current < 1 || current > ts->count) {
+        printf("invalid test number %d\n", current);
+        status = TEST_INVALID;
+    }
+    if (ts->results[current - 1] != TEST_INVALID) {
+        printf("duplicate test number %d\n", current);
+        status = TEST_INVALID;
+    }
+    ts->current = current;
     return status;
 }
 
@@ -160,53 +241,55 @@ test_run(struct testset *ts)
 {
     char buffer[BUFSIZ];
     FILE *output;
-    pid_t testpid;
-    int outfd, current, status, i;
+    pid_t testpid, child;
+    enum test_status result;
+    int outfd, status, i;
     int missing = 0;
     int failed = 0;
 
     /* Initialize the test and our data structures. */
-    testpid = test_start(ts->file, "errors.out", &outfd);
+    testpid = test_start(ts->file, &outfd);
     output = fdopen(outfd, "r");
     if (!output) sysdie("fdopen failed");
-    if (!fgets(buffer, sizeof(buffer), output)) sysdie("fgets failed");
-    if (!test_init(buffer, ts)) return 0;
+    if (!fgets(buffer, sizeof(buffer), output)) {
+        puts("read failure");
+        ts->aborted = 1;
+    }
+    if (!ts->aborted && !test_init(buffer, ts)) {
+        while (fgets(buffer, sizeof(buffer), output))
+            ;
+        ts->aborted = 1;
+    }
 
     /* Read each line the test prints to stdout, checking it for ok or not
-       ok and failing the test set if we see anything else. */
-    while (fgets(buffer, sizeof(buffer), output)) {
-        if (buffer[strlen(buffer) - 1] != '\n') {
-            warn("%s: excessive output: %s", ts->file, buffer);
-            return 0;
+       ok and failing the test set if we see anything odd. */
+    while (!ts->aborted && fgets(buffer, sizeof(buffer), output)) {
+        result = test_checkline(buffer, ts);
+        switch (result) {
+            case TEST_PASS:     ts->passed++;   break;
+            case TEST_FAIL:     ts->failed++;   break;
+            case TEST_SKIP:     ts->skipped++;  break;
         }
-        if (sscanf(buffer, "not ok %d", &current) == 1) {
-            status = TEST_FAIL;
-        } else if (sscanf(buffer, "ok %d", &current) == 1) {
-            status = TEST_PASS;
-        } else {
-            warn("%s: invalid output: %s", ts->file, buffer);
-            return 0;
-        }
-        if (current < 1 || current > ts->count) {
-            warn("%s: invalid test number %d", ts->file, current);
-            return 0;
-        }
-        if (ts->results[current - 1] != TEST_INVALID) {
-            warn("%s: duplicate test number %d", ts->file, current);
-        }
-        ts->results[current - 1] = status;
-        if (status == TEST_PASS) ts->passed++; else ts->failed++;
+        ts->results[ts->current - 1] = result;
     }
-    if (ferror(output)) sysdie("%s: error reading output");
-    status = test_finish(outfd, testpid);
-    if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-        warn("%s: non-zero exit status %d", ts->file, status);
-        return 0;
+    if (ferror(output)) {
+        puts("read error");
+        ts->aborted = 1;
     }
 
-    /* This closes the output file descriptor twice, but we want to free the
-       FILE structure as well and the second close should be harmless. */
+    /* Close the output descriptor, retrieve the exit status, and print out
+       the status if appropriate. */
     fclose(output);
+    child = waitpid(testpid, &ts->status, 0);
+    if (child == (pid_t) -1)
+        sysdie("waitpid for %u failed", (unsigned int) testpid);
+    if (!WIFEXITED(ts->status) || WEXITSTATUS(ts->status) != 0) {
+        if (!ts->aborted) puts("dubious");
+        fputs("  ", stdout);
+        wstat_print(ts->status);
+        return 0;
+    }
+    if (ts->aborted) return 0;
 
     /* Summarize the results of the test set to stdout. */
     for (i = 0; i < ts->count; i++)
@@ -219,7 +302,8 @@ test_run(struct testset *ts)
         }
     }
     if (!missing && !failed) fputs("ok", stdout);
-    return 1;
+    putchar('\n');
+    return (!missing && !failed);
 }
 
 
@@ -236,6 +320,8 @@ test_batch(const char *testlist)
     char buffer[BUFSIZ];
     int line, i;
     struct testset ts;
+    struct timeval start, end;
+    struct rusage stats;
     int total = 0;
     int failed = 0;
 
@@ -259,6 +345,9 @@ test_batch(const char *testlist)
     longest += 2;
     if (longest % 8) longest += 8 - (longest % 8);
 
+    /* Start the wall clock timer. */
+    gettimeofday(&start, NULL);
+
     /* Now, plow through our tests again, running each one.  Check line
        length again out of paranoia. */
     line = 0;
@@ -275,20 +364,25 @@ test_batch(const char *testlist)
         if (test_run(&ts)) {
             total += ts.count;
             failed += ts.failed;
-            putchar('\n');
-        } else {
-            printf("\nFailed!\n");
         }
     }
 
+    /* Stop the timer and get our child resource statistics. */
+    gettimeofday(&end, NULL);
+    getrusage(RUSAGE_CHILDREN, &stats);
+
     /* Print out our final results. */
     if (failed == 0) {
-        printf("All tests successful.\n");
+        puts("All tests successful.");
     } else {
         printf("Failed %d/%d tests, %.2f%% okay.\n", failed, total,
                (total - failed) * 100.0 / total);
     }
-    printf("Files=%d,  Tests=%d\n", line, total);
+    printf("Files=%d,  Tests=%d", line, total);
+    printf(",  %.2f seconds", tv_diff(&end, &start));
+    printf(" (%.2f usr + %.2f sys = %.2f CPU)\n",
+           tv_seconds(&stats.ru_utime), tv_seconds(&stats.ru_stime),
+           tv_sum(&stats.ru_utime, &stats.ru_stime));
     return (failed != 0);
 }
 
@@ -298,6 +392,5 @@ int
 main(int argc, char *argv[])
 {
     if (argc != 2) die("Usage: runtests <test-list>\n");
-    test_batch(argv[1]);
-    return 0;
+    return test_batch(argv[1]);
 }
