87172ca55c1f6a0628cd0ede253dcda10e8a12cc
Russ Allbery <rra@stanford.edu>
2000-06-15 09:48:25 +0000

    Redid the support for alternate error handlers.  Set up a default error handler that prints to stderr for both warn and die and allow setting of alternate error handlers via a function.

--- util/error.c
+++ util/error.c
@@ -22,13 +22,15 @@
 
         error_fatal_cleanup = cleanup;
         error_program_name = argv[0];
-        error_log_function = log;
 
         warn("Something horrible happened at %lu", time);
         syswarn("Couldn't unlink temporary file %s", tmpfile);
 
         die("Something fatal happened at %lu", time);
         sysdie("open of %s failed", filename);
+
+        warn_set_handlers(1, log);
+        warn("This now goes through our log function");
 
    warn prints an error followed by a newline to stderr.  die does the same
    but then exits, normally with a status of 1.  The sys* versions do the
@@ -44,12 +46,14 @@
    colon and a space, is prepended to all error messages.  It is a const
    char *.
 
-   If error_log_function is non-NULL, the number of octets the format and
-   its arguments produce when run through printf, the format, the arguments
-   (in the form of a va_list), and errno for syswarn or sysdie and 0
-   otherwise are passed to that function after the message is sent to
-   stderr.  The primary purpose of this function is to further output the
-   message to a file, to syslog, etc. */
+   Honoring error_program_name and printing to stderr is just the default
+   handler; with warn_set_handlers or die_set_handlers the handlers for warn
+   and die can be changed.  These functions take a count of handlers and
+   then that many function pointers, each one to a function that takes a
+   message length (or 0 if unknown), a format, an argument list as a
+   va_list, and the applicable errno value (if any).  They should return the
+   number of octets that the format combined with the arguments produces
+   when passed through printf (the message length) or 0 if unknown. */
 
 #include "config.h"
 #include "librutil.h"
@@ -62,9 +66,15 @@
 # include <string.h>
 #endif
 
-/* If non-NULL, called after the log message is sent to stderr and passed a
-   copy of the format, the arguments, and some other information. */
-error_log_t error_log_function = 0;
+/* The default logging function, which prints to stderr. */
+static int logerr(int length, const char *format, va_list args, int error);
+
+/* The default handler list. */
+static error_handler_t default_handlers[2] = { logerr, 0 };
+
+/* The list of logging functions currently in effect. */
+static error_handler_t *die_handlers  = default_handlers;
+static error_handler_t *warn_handlers = default_handlers;
 
 /* If non-NULL, called before exit and its return value passed to exit. */
 int (*error_fatal_cleanup)(void) = 0;
@@ -72,21 +82,60 @@ int (*error_fatal_cleanup)(void) = 0;
 /* If non-NULL, prepended (followed by ": ") to all error messages. */
 const char *error_program_name = 0;
 
+
+void
+warn_set_handlers(int count, ...)
+{
+    va_list args;
+    int i;
+
+    if (warn_handlers != default_handlers) free(warn_handlers);
+    warn_handlers = xmalloc(sizeof(error_handler_t) * (count + 1));
+    va_start(args, count);
+    for (i = 0; i < count; i++)
+        warn_handlers[i] = (error_handler_t) va_arg(args, error_handler_t);
+    va_end(args);
+    warn_handlers[count] = 0;
+}
+
+void
+die_set_handlers(int count, ...)
+{
+    va_list args;
+    int i;
+
+    if (die_handlers != default_handlers) free(die_handlers);
+    die_handlers = xmalloc(sizeof(error_handler_t) * (count + 1));
+    va_start(args, count);
+    for (i = 0; i < count; i++)
+        die_handlers[i] = (error_handler_t) va_arg(args, error_handler_t);
+    va_end(args);
+    die_handlers[count] = 0;
+}
+
+
+static int
+logerr(int length, const char *format, va_list args, int error)
+{
+    fflush(stdout);
+    if (error_program_name) fprintf(stderr, "%s: ", error_program_name);
+    length = vfprintf(stderr, format, args);
+    if (error) fprintf(stderr, ": %s", strerror(error));
+    fprintf(stderr, "\n");
+    return length;
+}
+
+
 void
 warn(const char *format, ...)
 {
     va_list args;
-    int length;
+    error_handler_t *log;
+    int length = 0;
 
-    fflush(stdout);
-    if (error_program_name) fprintf(stderr, "%s: ", error_program_name);
-    va_start(args, format);
-    length = vfprintf(stderr, format, args);
-    va_end(args);
-    fprintf(stderr, "\n");
-    if (error_log_function) {
+    for (log = warn_handlers; *log; log++) {
         va_start(args, format);
-        (*error_log_function)(length, format, args, 0);
+        length = (**log)(length, format, args, 0);
         va_end(args);
     }
 }
@@ -95,18 +144,13 @@ void
 syswarn(const char *format, ...)
 {
     va_list args;
-    int length;
+    error_handler_t *log;
+    int length = 0;
     int error = errno;
 
-    fflush(stdout);
-    if (error_program_name) fprintf(stderr, "%s: ", error_program_name);
-    va_start(args, format);
-    length = vfprintf(stderr, format, args);
-    va_end(args);
-    fprintf(stderr, ": %s\n", strerror(error));
-    if (error_log_function) {
+    for (log = warn_handlers; *log; log++) {
         va_start(args, format);
-        (*error_log_function)(length, format, args, error);
+        length = (**log)(length, format, args, error);
         va_end(args);
     }
 }
@@ -115,17 +159,12 @@ void
 die(const char *format, ...)
 {
     va_list args;
-    int length;
+    error_handler_t *log;
+    int length = 0;
 
-    fflush(stdout);
-    if (error_program_name) fprintf(stderr, "%s: ", error_program_name);
-    va_start(args, format);
-    length = vfprintf(stderr, format, args);
-    va_end(args);
-    fprintf(stderr, "\n");
-    if (error_log_function) {
+    for (log = die_handlers; *log; log++) {
         va_start(args, format);
-        (*error_log_function)(length, format, args, 0);
+        length = (**log)(length, format, args, 0);
         va_end(args);
     }
     exit(error_fatal_cleanup ? (*error_fatal_cleanup)() : 1);
@@ -135,18 +174,13 @@ void
 sysdie(const char *format, ...)
 {
     va_list args;
-    int length;
+    error_handler_t *log;
+    int length = 0;
     int error = errno;
 
-    fflush(stdout);
-    if (error_program_name) fprintf(stderr, "%s: ", error_program_name);
-    va_start(args, format);
-    length = vfprintf(stderr, format, args);
-    va_end(args);
-    fprintf(stderr, ": %s\n", strerror(error));
-    if (error_log_function) {
+    for (log = die_handlers; *log; log++) {
         va_start(args, format);
-        (*error_log_function)(length, format, args, error);
+        length = (**log)(length, format, args, error);
         va_end(args);
     }
     exit(error_fatal_cleanup ? (*error_fatal_cleanup)() : 1);
