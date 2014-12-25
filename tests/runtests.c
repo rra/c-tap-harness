decf4b35b9129310f24cb0e1c11cb8e3cd1cab1d
Russ Allbery <eagle@eyrie.org>
2014-12-25 13:29:08 -0800

    Support comments and blank lines in test lists
    The runtests harness now supports ignoring comments and blank lines in
    the test list specified with -l.  Leading whitespace before the test
    name is also ignored.

--- tests/runtests.c
+++ tests/runtests.c
@@ -1175,6 +1175,7 @@ read_test_list(const char *filename)
     unsigned int line;
     size_t length;
     char buffer[BUFSIZ];
+    const char *testname;
     struct testlist *listhead, *current;
 
     /* Create the initial container list that will hold our results. */
@@ -1197,6 +1198,15 @@ read_test_list(const char *filename)
             exit(1);
         }
         buffer[length] = '\0';
+
+        /* Skip comments, leading spaces, and blank lines. */
+        testname = skip_whitespace(buffer);
+        if (strlen(testname) == 0)
+            continue;
+        if (testname[0] == '#')
+            continue;
+
+        /* Allocate the new testset structure. */
         if (current == NULL)
             current = listhead;
         else {
@@ -1205,7 +1215,7 @@ read_test_list(const char *filename)
         }
         current->ts = xcalloc(1, sizeof(struct testset));
         current->ts->plan = PLAN_INIT;
-        current->ts->file = xstrdup(buffer);
+        current->ts->file = xstrdup(testname);
     }
     fclose(file);
 
